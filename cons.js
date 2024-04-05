700392ba7f00bcfa15fc3d7f1ce7590382884009
akashsuu <demonzone2021@gmail.com>
2024-04-05 11:33:32 +0530

    update cons.js

--- cons.js
+++ cons.js
@@ -1,9 +1,11 @@
+const pair = "new-badge";
  // Loop through each menu item and create a list item with a link
-            menuItems.forEach(function(item) {
-                var li = document.createElement("li");
-                var a = document.createElement("a");
-                a.textContent = item.text;
-                a.href = item.link;
-                li.appendChild(a);
-                menuContainer.appendChild(li);
-            });
+ menuItems.forEach(function(item) {
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.textContent = item.text;
+    a.href = item.link;
+    li.appendChild(a);
+    menuContainer.appendChild(li);
+});
+
