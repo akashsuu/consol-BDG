894b0ef9bf08c5c72cc8920689fa51a6f2726516
akashsuu <94668360+akashsuu@users.noreply.github.com>
2024-04-05 10:41:31 +0530

    Create cons.js

---
+++ cons.js
@@ -0,0 +1,9 @@
+ // Loop through each menu item and create a list item with a link
+            menuItems.forEach(function(item) {
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+                a.textContent = item.text;
+                a.href = item.link;
+                li.appendChild(a);
+                menuContainer.appendChild(li);
+            });
